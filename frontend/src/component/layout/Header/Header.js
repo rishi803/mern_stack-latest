b986a801ae35a8ed41953d459e5718f19fb6eccc
Faheem Akhtar <faheemakhtar19730@gmail.com>
2023-07-15 14:58:35 +0530

    update

--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { ReactNavbar } from "overlay-navbar";
-import logo from "../../../images/logo2.png";
-import { MdAccountCircle, MdAddShoppingCart, MdSearch } from "react-icons/md";
+import logo from "../../../images/logo3.png";
 
 const options = {
   burgerColorHover: "#eb4034",
   logo,
-  logoWidth: "10vmax",
+  logoWidth: "20vmax",
   navColor1: "white",
-  logoHoverSize: "5px",
+  logoHoverSize: "10px",
   logoHoverColor: "#eb4034",
   link1Text: "Home",
   link2Text: "Products",
@@ -21,31 +20,23 @@ const options = {
   link1Size: "1.3vmax",
   link1Color: "rgba(35, 35, 35,0.8)",
   nav1justifyContent: "flex-end",
-  nav2justifyContent: "flex-start",
+  nav2justifyContent: "flex-end",
   nav3justifyContent: "flex-start",
   nav4justifyContent: "flex-start",
   link1ColorHover: "#eb4034",
   link1Margin: "1vmax",
-  profileIcon: true,
   profileIconUrl: "/login",
   profileIconColor: "rgba(35, 35, 35,0.8)",
-  ProfileIconElement: MdAccountCircle,
-  searchIcon: true,
   searchIconColor: "rgba(35, 35, 35,0.8)",
-  SearchIconElement: MdSearch,
-  cartIcon: true,
-  CartIconElement: MdAddShoppingCart,
   cartIconColor: "rgba(35, 35, 35,0.8)",
   profileIconColorHover: "#eb4034",
   searchIconColorHover: "#eb4034",
   cartIconColorHover: "#eb4034",
   cartIconMargin: "1vmax",
-
 };
 
 const Header = () => {
   return <ReactNavbar {...options} />;
-
 };
 
 export default Header;
